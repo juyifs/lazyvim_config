aecc010301e9353c8960634bfac32e5216c3ed49
CHENZ008 <zhijie.chen@forvia.com>
2026-05-14 11:26:25 +0800

    更新snacks picker边框样式

--- init.lua
+++ init.lua
@@ -7,3 +7,10 @@ vim.api.nvim_set_hl(0, "CursorLine", { link = "Visual", bold = true })
 -- 如果你希望行号部分的当前行颜色也同步改变，可以同时链接 CursorLineNr
 vim.api.nvim_set_hl(0, "CursorLineNr", { link = "Visual", bold = true })
 
+-- 链接到标准浮动窗口边框（推荐，最为和谐）
+vim.api.nvim_set_hl(0, "SnacksPickerBorder", { link = "FloatBorder" })
+-- 如果阁下希望输入框、列表框、预览框的分割线颜色完全一致：
+vim.api.nvim_set_hl(0, "SnacksPickerInputBorder", { link = "FloatBorder" })
+vim.api.nvim_set_hl(0, "SnacksPickerListBorder", { link = "FloatBorder" })
+vim.api.nvim_set_hl(0, "SnacksPickerPreviewBorder", { link = "FloatBorder" })
+
