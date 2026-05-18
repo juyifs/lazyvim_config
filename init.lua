1b0e9220cd62cbdbecdf4516b6e86c04248e6f61
CHENZ008 <zhijie.chen@forvia.com>
2026-05-18 18:10:10 +0800

    修改当前高亮行颜色为浅蓝色

--- init.lua
+++ init.lua
@@ -2,10 +2,10 @@
 require("config.lazy")
 
 -- 将当前行背景色链接到选中（Visual）高亮组
-vim.api.nvim_set_hl(0, "CursorLine", { link = "Visual", bold = true })
+vim.api.nvim_set_hl(0, "CursorLine", { bg = "#EBF3F9", bold = true })
 
 -- 如果你希望行号部分的当前行颜色也同步改变，可以同时链接 CursorLineNr
-vim.api.nvim_set_hl(0, "CursorLineNr", { link = "Visual", bold = true })
+vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "#EBF3F9", bold = true })
 
 -- 链接到标准浮动窗口边框（推荐，最为和谐）
 vim.api.nvim_set_hl(0, "SnacksPickerBorder", { link = "FloatBorder" })
